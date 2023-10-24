87dc1ea87b0be71d11013fbd3540e67e15f3206a
Joshua Duenow <joshjduenow@gmail.com>
2023-10-23 19:52:08 -0500

    fixed some spacing issue

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ myScrabbleTiles.push(
   { tile: 'K', score: 5 },
   { tile: 'Z', score: 10 },
   { tile: 'X', score: 8 },
-  {tile: 'D', score: 2 },
+  { tile: 'D', score: 2 },
   { tile: 'A', score: 1 },
 
 );
